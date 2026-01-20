6d3429502910201d10410ae336ea969e5dcbabfb
BenDang0309 <bendang0309@gmail.com>
2026-01-20 17:16:23 -0500

    Update Client.java

--- Client/Client.java
+++ Client/Client.java
@@ -1,6 +1,27 @@
 import java.io.* ;
 import java.net.* ;
+import javax.swing.SwingUtilities;
 
 public class Client {
-  private static final int PORT = 
+  private static final int PORT = 5000;
+
+  private static Jlabel errorLabel
+  public int x, y;
+  public String color;
+  public String message;
+  public boolean pinned;
+
+  public static void main(String[] argv) {
+    SwingUtilities.invokeLater(() -> {
+        try {
+            host = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            host = "localhost";  
+            System.err.println("Could not determine local host address: " + e.getMessage());
+        }
+        openGUI();
+    });
+    
+  }
+  
 }
