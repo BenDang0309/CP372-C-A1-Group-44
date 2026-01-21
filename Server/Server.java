bcac9fec49813eea9bc6ccb3c964e49fa3e6ecf4
BenDang0309 <bendang0309@gmail.com>
2026-01-20 23:55:26 -0500

    Update Server.java

--- Server/Server.java
+++ Server/Server.java
@@ -10,18 +10,34 @@ public final class Server {
   public static void main(String argv[]) {
     int port = 1000;
     if (argv.length > 0) {
-			try {
-				port = Integer.valueOf(argv[0]);
-			}
-			catch (NumberFormatException e) {
-				System.out.println("invalid port, defaulting to 1000");
-			}
+		try {
+			port = Integer.valueOf(argv[0]);
 		}
-		else {
-			System.out.println("User didn't enter port, defaulting to 1000");
+		catch (NumberFormatException e) {
+			System.out.println("invalid port, defaulting to 1000");
 		}
+	}
+	else {
+		System.out.println("User didn't enter port, defaulting to 1000");
+	}
 
     System.out.println("Server starting in port number: " + port);
-    
-  }
+	// listen socket
+	ServerSocket socket;
+	try { 
+		socket = new ServerSocket(port);
+	}
+	catch (IOException e) {
+		System.out.println("User didn't enter port, defaulting to 1000");
+		port = 1000;
+		socket = new ServerSocket(port);
+	}
+
+	// infinite loop to process services
+	while (true) {
+		Socket connection = socket.accept(); // listen for TCP connection request
+		Conversation request = new Conversation(connection,notes); // connection between client and server
+		Thread thread = new Thread(request); // new thread to process the request
+		thread.start();
+  	}
 }
