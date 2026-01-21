50107a26d5d99c60515e84d748d1ed943cef74e6
BenDang0309 <bendang0309@gmail.com>
2026-01-20 23:27:51 -0500

    Update Server.java
    added a note class and psvm

--- Server/Server.java
+++ Server/Server.java
@@ -1,1 +1,34 @@
+import java.io.* ;
+import java.net.* ;
+import java.util.* ;
+import java.util.concurrent.*;
 
+public final class Server {
+  // Java concurrenthashmap for multi-threading
+  public static ConcurrentHashMap<String, Note> notes = new ConcurrentHashMap<>(); // stores the notes by their coordinates
+  
+  public static void main(String argv[]) {
+    int port = 1000;
+    if (argv.length > 0) {
+			try {
+				port = Integer.valueOf(argv[0]);
+			}
+			catch (NumberFormatException e) {
+				System.out.println("invalid port, defaulting to 1000");
+			}
+		}
+		else {
+			System.out.println("User didn't enter port, defaulting to 1000");
+		}
+
+    System.out.println("Server starting in port number: " + port)
+    
+  }
+  public static class Note {
+    public final int color;
+    public final string message;
+    public final int x;
+    public final int y;
+    public boolean pinned;
+  }
+}
