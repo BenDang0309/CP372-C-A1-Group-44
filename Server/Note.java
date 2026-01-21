03768d0ff7b878e6faa6bff8020ad7483a92d555
BenDang0309 <bendang0309@gmail.com>
2026-01-20 23:35:31 -0500

    Update Note.java

--- Server/Note.java
+++ Server/Note.java
@@ -1,1 +1,17 @@
+import java.util.* ;
 
+public class Note {
+  public final int color;
+  public final String message;
+  public final int x;
+  public final int y;
+  public boolean pinned;
+
+  public Note(int x, int y, String color, String message) {
+    this.color = color;
+    this.message = message;
+    this.x = x;
+    this.y = y;
+    this.pinned = pinned;
+  }
+}
