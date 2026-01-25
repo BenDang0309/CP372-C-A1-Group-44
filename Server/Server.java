acbff10fb01e35936ecc04cd8b9aa52b16bf2c4a
BenDang0309 <bendang0309@gmail.com>
2026-01-25 14:21:45 -0500

    Switched to thread pools because it reuses threads which is more efficient, and to use pool.shutdown.

--- Server/Server.java
+++ Server/Server.java
@@ -9,35 +9,37 @@ public final class Server {
   
   public static void main(String argv[]) {
     int port = 1000;
+
     if (argv.length > 0) {
-		try {
-			port = Integer.valueOf(argv[0]);
-		}
-		catch (NumberFormatException e) {
-			System.out.println("invalid port, defaulting to 1000");
-		}
-	}
-	else {
-		System.out.println("User didn't enter port, defaulting to 1000");
-	}
+        try {
+            port = Integer.valueOf(argv[0]);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("invalid port, defaulting to 1000");
+        }
+    }
+    else {
+        System.out.println("User didn't enter port, defaulting to 1000");
+    }
 
-    System.out.println("Server starting in port number: " + port);
-	// listen socket
-	ServerSocket socket;
-	try { 
-		socket = new ServerSocket(port);
-	}
-	catch (IOException e) {
-		System.out.println("User didn't enter port, defaulting to 1000");
-		port = 1000;
-		socket = new ServerSocket(port);
-	}
+    ExecutorService pool = Executors.newCachedThreadPool(); // pool of threads
 
-	// infinite loop to process services
-	while (true) {
-		Socket connection = socket.accept(); // listen for TCP connection request
-		Conversation request = new Conversation(connection,notes); // connection between client and server
-		Thread thread = new Thread(request); // new thread to process the request
-		thread.start();
-  	}
+    try (ServerSocket socket = new ServerSocket(port)) {
+        System.out.println("Server starting at port " + port);
+        // process services
+        while (true) {
+            try {
+                Socket connection = socket.accept(); // listen for TCP connection request
+                pool.submit(new Conversation(connection)); // runs the conversation task on one of the threads in the pool
+            }
+            catch (IOException acceptEx) {
+                System.err.println("Accept failed: " + acceptEx.getMessage());
+            }
+        }
+    } catch (IOException e) {
+        System.err.println("Could not start server on port " + port + ": " + e.getMessage());
+    } finally {
+        pool.shutdown();
+    }
+  }
 }
