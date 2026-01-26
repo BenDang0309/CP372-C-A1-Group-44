780b7072940cf3a5eed1481cd306c473d30f2ae6
Arman Bhullar <arman04@live.ca>
2026-01-26 14:09:50 -0500

    Add socket connection and command handling methods

--- Client/Client.java
+++ Client/Client.java
@@ -3,5 +3,23 @@ import java.net.* ;
 import javax.swing.SwingUtilities;
 
 public class Client {
-  
+
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public void connect(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public String sendCommand(String command) throws IOException {
+        out.println(command);
+        return in.readLine();
+    }
+
+    public void disconnect() throws IOException {
+        if (socket != null) socket.close();
+    }
 }
