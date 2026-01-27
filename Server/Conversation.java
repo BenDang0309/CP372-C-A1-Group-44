920aad5430cece376f65fe2c6b9ddf12b5a81d3c
BenDang0309 <bendang0309@gmail.com>
2026-01-26 19:00:20 -0500

    Update Conversation.java

--- Server/Conversation.java
+++ Server/Conversation.java
@@ -3,6 +3,43 @@ import java.net.* ;
 import java.util.* ;
 import java.util.concurrent.*;
 
-final class Conversation {
+final class Conversation implements Runnable {
+  Socket socket;
+  ConcurrentHashMap<String, Note> notes;
 
+  // constructor
+  public Conversation(Socket socket, ConcurrentHashMap<String, Note> notes) {
+    this.socket = socket;
+    this.notes = notes;
+  }
+  
+  public void run() {
+    try {
+      process();
+    }
+    catch (Exception e) {
+      println("Exception: " + e);
+    }
+  }
+
+  private void process() {
+    System.out.println("Processing request");
+    
+    Scanner input = new Scanner(socket.getInputStream()); // take in input
+    PrintWriter out = new PrintWriter(socket.getOutputStream(), true); // write out output
+    String cmd = input.nextLine(); // command
+    String parts[] = cmd.split(" ", 5); // splitting the command up into parts for processing
+    String op = parts[0]; // the operation
+
+    System.out.println("Operation: " + op);
+
+    if (op.equals("POST") {
+      int x = Integer.parseInt(parts[1]);
+      int y = Integer.parseInt(parts[2]);
+      String color = parts[3];
+      String message = parts[4];
+      
+    }
+    
+  }
 }
