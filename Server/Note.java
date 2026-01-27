d4001c90b4290c7d5a2872e2cf945fc404681212
BenDang0309 <bendang0309@gmail.com>
2026-01-27 16:31:09 -0500

    added operations, helpers and getters

--- Server/Note.java
+++ Server/Note.java
@@ -18,4 +18,48 @@ public class Note {
     this.height = height;
     this.pinCount = 0;
   }
+
+  // add pin
+  public synchronized void pin() {
+    pinCount++;
+  }
+
+  // remove pin but check if there is a pin to remove
+  public synchronized boolean unpin() {
+    if (pinCount == 0) {
+      return false;
+    }
+    pinCount--;
+    return true;
+  }
+
+  // get number of pins
+  public synchronized int getPinCount() {
+    return pinCount;
+  }
+
+  // check if there is a pin, for shake
+  public synchronized boolean isPinned() {
+    return pinCount > 0;
+  }
+
+  // check if a note completely overlaps with another note
+  public boolean overlap(int ox, int oy, int ow, int oh) {
+    return this.x == ox && this.y == oy && this.width == ow && this.height == oh;
+  }
+
+  // check whether the point is inside the note coordinates, for pin/unpin
+  public boolean containsPoint(int cx, int cy) {
+    return (cx >= x) && (cx < x + width) && (cy >= y) && (cy < y + height);
+  }
+
+  // getters
+  public String getId() { return id; }
+  public String getColor() { return color; }
+  public String getMessage() { return message; }
+  public int getX() { return x; }
+  public int getY() { return y; }
+  public int getWidth() { return width; }
+  public int getHeight() { return height; }
+  
 }
